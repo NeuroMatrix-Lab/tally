1b9887432d14253f3f73a5131cd1f966fe238ab4
Tally Developer <noreply@users.noreply.github.com>
2026-04-01 09:23:18 +0800

    #
    无用依赖清理

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'models/record.dart';
